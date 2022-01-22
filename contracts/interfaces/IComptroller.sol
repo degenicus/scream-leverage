96e074a23d61be920487c7f9e55aa9f018293c3b
Magnus Brantheim <m.brantheim@gmail.com>
2022-01-22 16:52:59 +0000

    Fix merge problem

--- contracts/interfaces/IComptroller.sol
+++ contracts/interfaces/IComptroller.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.9;
 import './CTokenI.sol';
 interface IComptroller {
     function compAccrued(address user) external view returns (uint256 amount);
-    function claimComp(address holder, address[] calldata _scTokens) external;
+    function claimComp(address holder, CTokenI[] memory _scTokens) external;
     function claimComp(address holder) external;
     function enterMarkets(address[] memory _scTokens) external;
     function pendingComptrollerImplementation() view external returns (address implementation);
