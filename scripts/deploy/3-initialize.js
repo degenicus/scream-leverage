9c9dd3937c26698458f2b0c97058bc3cdaa737eb
Magnus Brantheim <m.brantheim@gmail.com>
2022-02-16 22:21:29 +0000

    Deploy FRAX

--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = '0xC2cE269f3646a5F5bF1cCDa73c6cAB50f64012b6';
-  const strategyAddress = '0xb2C1De1f7f03054BDCbeC35f9f8370C09719fF1D';
+  const vaultAddress = '0x089fF2BaC6F5610ee3b54eF9cf13E6E196488dAB';
+  const strategyAddress = '0x2a4a7B7AC87a416aE83772fEd196259A5fd47C63';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_3');
   const vault = Vault.attach(vaultAddress);
