6223b017e44c28ec7883b79e4ad7a4673090c18e
Magnus Brantheim <m.brantheim@gmail.com>
2022-03-02 19:34:45 +0000

    Upgrade wbtc with withdraw bug fix

--- scripts/upgradeProxy.js
+++ scripts/upgradeProxy.js
@@ -1,16 +1,16 @@
-const tusdProxy = '';
-const options = { gasPrice: 1000000000000 };
+const wbtcProxy = '0x49D77552b4710bBf2F8176835e08BE885744734c';
+const options = { gasPrice: 700000000000 };
 const targetLTV = ethers.utils.parseEther('0.72');
 
 const getStrategy = async () => {
   const Strategy = await ethers.getContractFactory('ReaperAutoCompoundScreamLeverage');
-  const strategy = Strategy.attach(tusdProxy);
+  const strategy = Strategy.attach(wbtcProxy);
   return strategy;
 };
 
 const upgradeProxy = async () => {
   const stratFactory = await ethers.getContractFactory('ReaperAutoCompoundScreamLeverage');
-  await hre.upgrades.upgradeProxy(tusdProxy, stratFactory, { ...options, timeout: 0 });
+  await hre.upgrades.upgradeProxy(wbtcProxy, stratFactory, { ...options, timeout: 0 });
   console.log('upgradeProxy');
 };
 
@@ -39,11 +39,11 @@ const setTargetLTV = async () => {
 };
 
 async function main() {
-  await upgradeProxy();
+  //await upgradeProxy();
   //await clearUpgradeCooldown();
   //await setSlippage();
   //await setTargetLTV();
-  //await unpause();
+  await unpause();
 }
 
 main()
