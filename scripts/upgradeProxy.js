0ee07133f88c3fdc5837266aa65788d142cd8ebd
Bastien Guyonnet <bastien.guyonnet@tutanota.com>
2022-03-03 10:53:21 +0100

    mim-upgrade-3/3/22

--- scripts/upgradeProxy.js
+++ scripts/upgradeProxy.js
@@ -1,16 +1,22 @@
-const tusdProxy = '';
+const mimProxy = '0x7340d1F5F5C4f6a72F2cAbE7881e5DdeFEA707fF';
+const mimVault = '0xCA55757854222d8232a19EC8Aae336594eE3b5E5';
 const options = { gasPrice: 1000000000000 };
 const targetLTV = ethers.utils.parseEther('0.72');
 
 const getStrategy = async () => {
   const Strategy = await ethers.getContractFactory('ReaperAutoCompoundScreamLeverage');
-  const strategy = Strategy.attach(tusdProxy);
+  const strategy = Strategy.attach(mimProxy);
   return strategy;
 };
 
+const getVault = async () => {
+  const Vault = await ethers.getContractFactory('ReaperVaultv1_3');
+  return Vault.attach(mimVault);
+}
+
 const upgradeProxy = async () => {
   const stratFactory = await ethers.getContractFactory('ReaperAutoCompoundScreamLeverage');
-  await hre.upgrades.upgradeProxy(tusdProxy, stratFactory, { ...options, timeout: 0 });
+  await hre.upgrades.upgradeProxy(mimProxy, stratFactory, { ...options, timeout: 0 });
   console.log('upgradeProxy');
 };
 
@@ -38,12 +44,19 @@ const setTargetLTV = async () => {
   console.log('setTargetLTV');
 };
 
+const earn = async () => {
+  const vault = await getVault();
+  await vault.earn();
+  console.log('earn');
+};
+
 async function main() {
-  await upgradeProxy();
-  //await clearUpgradeCooldown();
-  //await setSlippage();
-  //await setTargetLTV();
-  //await unpause();
+  // await upgradeProxy();
+  // await clearUpgradeCooldown();
+  // await setSlippage();
+  // await setTargetLTV();
+  // await unpause();
+  await earn();
 }
 
 main()
