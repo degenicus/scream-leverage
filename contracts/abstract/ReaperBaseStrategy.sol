e8b9d0fdded8e42a304308402ecabef03f7cc0ca
tess3rac7 <tess3rac7@protonmail.com>
2022-03-30 16:23:34 -0400

    Upgrade timelock is 48 hours.

--- contracts/abstract/ReaperBaseStrategy.sol
+++ contracts/abstract/ReaperBaseStrategy.sol
@@ -17,7 +17,7 @@ abstract contract ReaperBaseStrategy is
 {
     uint256 public constant PERCENT_DIVISOR = 10_000;
     uint256 public constant ONE_YEAR = 365 days;
-    uint256 public constant UPGRADE_TIMELOCK = 1 hours; // minimum 48 hours for RF
+    uint256 public constant UPGRADE_TIMELOCK = 48 hours; // minimum 48 hours for RF
 
     struct Harvest {
         uint256 timestamp;
