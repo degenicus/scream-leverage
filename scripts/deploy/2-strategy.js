4ec64156b2f4b69ea88ccd0790234fd73bfa4914
Magnus Brantheim <m.brantheim@gmail.com>
2022-04-04 18:39:19 +0200

    Deployed ust with 6 decimal vault

--- scripts/deploy/2-strategy.js
+++ scripts/deploy/2-strategy.js
@@ -1,7 +1,7 @@
 const hre = require('hardhat');
 
 async function main() {
-  const vaultAddress = '';
+  const vaultAddress = '0x2415EFaeEf98C62a8D71F8B2454517885B180812';
 
   const Strategy = await ethers.getContractFactory('ReaperAutoCompoundScreamLeverage');
   const treasuryAddress = '0x0e7c5313E9BB80b654734d9b7aB1FB01468deE3b';
@@ -11,7 +11,7 @@ async function main() {
   const strategist3 = '0x1A20D7A31e5B3Bc5f02c8A146EF6f394502a10c4';
   const scUST = '';
 
-  const options = { gasPrice: 2000000000000, gasLimit: 9000000 };
+  const options = { gasPrice: 150000000000, gasLimit: 9000000 };
 
   const strategy = await hre.upgrades.deployProxy(
     Strategy,
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,10 +1,10 @@
 async function main() {
-  const vaultAddress = '';
-  const strategyAddress = '';
+  const vaultAddress = '0x2415EFaeEf98C62a8D71F8B2454517885B180812';
+  const strategyAddress = '0x8F3b2812EDafa0D020942b1cd16b554541FfE46C';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_3');
   const vault = Vault.attach(vaultAddress);
-  const options = { gasPrice: 2000000000000, gasLimit: 9000000 };
+  const options = { gasPrice: 150000000000, gasLimit: 9000000 };
 
   await vault.initialize(strategyAddress, options);
   console.log('Vault initialized');
